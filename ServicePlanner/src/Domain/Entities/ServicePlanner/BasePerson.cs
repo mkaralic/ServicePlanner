f5a8dc9f37bdb9fe4f482535ea7f67254c13c554
Milorad Karalic <miki.karalic@gmail.com>
2023-06-19 00:41:41 +0200

    ServicePlanner Entities and DbContext update with them

---
+++ ServicePlanner/src/Domain/Entities/ServicePlanner/BasePerson.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicePlanner.Domain.Entities.ServicePlanner;
+
+public abstract class BasePerson : BaseAuditableEntity
+{
+    [MaxLength(30)]
+    public string FirstName { get; set; }
+
+    [MaxLength(50)]
+    public string LastName { get; set; }
+
+    [MaxLength(20)]
+    public string Phone { get; set; }
+
+    [MaxLength(200)]
+    public string Address { get; set; }
+
+    [MaxLength(50)]
+    public string City { get; set; }
+
+    [MaxLength(50)]
+    public string Country { get; set; }
+
+    [Column(TypeName = "Text")]
+    public string Notes { get; set; }
+}
---
+++ ServicePlanner/src/Domain/Entities/ServicePlanner/Client.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace ServicePlanner.Domain.Entities.ServicePlanner;
+
+[Table("Client", Schema = "ServicePlanner")]
+public class Client : BasePerson
+{
+    public IEnumerable<WorkOrder> WorkOrders { get; set; }
+}
