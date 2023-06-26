fc43fb8185eb2fc1348f5dfa84f3a267b8cb71cb
Milorad Karalic <miki.karalic@gmail.com>
2023-06-27 01:00:52 +0200

    Client entity removed

--- ServicePlanner/src/Domain/Entities/ServicePlanner/Client.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-namespace ServicePlanner.Domain.Entities.ServicePlanner;
-
-[Table("Client", Schema = "ServicePlanner")]
-public class Client : BasePerson
-{
-    public IEnumerable<WorkOrder> WorkOrders { get; set; }
-}
---
+++ ServicePlanner/src/Infrastructure/Persistence/Migrations/20230626230015_ClientEntityRemoved.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServicePlanner.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClientEntityRemoved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
