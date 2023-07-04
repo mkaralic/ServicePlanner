8eab923ffdf72569263c68a813566d5e8624862b
Milorad Karalic <miki.karalic@gmail.com>
2023-07-05 01:20:41 +0200

    WorkOrder Description and Total Properties

--- ServicePlanner/src/Domain/Entities/ServicePlanner/WorkOrder.cs
+++ ServicePlanner/src/Domain/Entities/ServicePlanner/WorkOrder.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 
 namespace ServicePlanner.Domain.Entities.ServicePlanner;
@@ -13,6 +14,12 @@ public class WorkOrder : BaseAuditableEntity
     public Customer Customer { get; set; }
     public int? EmployeeId { get; set; }
     public Employee Employee { get; set; }
+
+    [MaxLength(50)]
+    public string ServiceDescription { get; set; }
+
+    [Column(TypeName = "decimal(18, 2)")] 
+    public decimal Total { get; set; }
 
     [Column(TypeName = "Text")]
     public string? Notes { get; set; }
---
+++ ServicePlanner/src/Infrastructure/Persistence/Migrations/20230704231939_WO_DescriptionAndTotal.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServicePlanner.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class WODescriptionAndTotal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ServiceDescription",
+                schema: "ServicePlanner",
+                table: "WorkOrders",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Total",
+                schema: "ServicePlanner",
+                table: "WorkOrders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ServiceDescription",
+                schema: "ServicePlanner",
+                table: "WorkOrders");
+
+            migrationBuilder.DropColumn(
+                name: "Total",
+                schema: "ServicePlanner",
+                table: "WorkOrders");
+        }
+    }
+}
